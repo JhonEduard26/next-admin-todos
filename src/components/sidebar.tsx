1ee173c30ee680c41232748c26f6db0a71c549df
jhon <jhon.bocanegra26@gmail.com>
2024-11-07 13:15:10 -0500

    feat: add Google provider to NextAuth, update sidebar for user session handling, and implement sign-out functionality

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,23 +1,46 @@
+import Image from "next/image"
+import { redirect } from "next/navigation"
+
+import { auth, signOut } from "@/auth"
 import LogoIcon from "@/icons/logo-icon"
 import NavLinks from "./nav-links"
+import ExitIcon from "@/icons/exit"
 
-export default function Sidebar() {
+export default async function Sidebar() {
+  const session = await auth()
+
+  if (!session?.user) {
+    redirect("/api/auth/signin")
+  }
+
   return (
-    <aside className="absolute top-0 left-0 h-screen w-52 p-5 border border-gray-300 bg-white">
+    <aside className="absolute top-0 left-0 flex flex-col h-screen w-52 p-5 border border-gray-300 bg-white">
       <div className="flex items-center gap-x-2 mb-10">
         <LogoIcon className="text-white w-10 h-10" />
         <h2 className="font-semibold">To-Do App</h2>
       </div>
       <div className="flex flex-col items-center mb-10">
-        <img
-          className="rounded-full w-28 h-28 mb-5"
-          src="https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250"
-          alt="user"
+        <Image
+          className="rounded-full mb-5 border-2 border-orange-500"
+          src={session.user.image!}
+          alt="User profile image"
+          width={112}
+          height={112}
         />
-        <p className="font-semibold">John Doe</p>
+        <p className="font-semibold text-center">{session.user.name}</p>
         <p className="text-sm text-gray-700">Role</p>
       </div>
       <NavLinks />
+      <div className="flex-1"></div>
+      <form action={async () => {
+        "use server"
+        await signOut()
+      }}>
+        <button className="button" type="submit">
+          <ExitIcon className="w-5 h-5" />
+          <span>Sign out</span>
+        </button>
+      </form>
     </aside>
   )
 }
