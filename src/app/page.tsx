47229dc5c2ae13ea5ca70f0ce5a6c33dd6d48283
jhon <jhon.bocanegra26@gmail.com>
2024-11-07 16:40:21 -0500

    feat: integrate Prisma adapter for NextAuth, update database schema for authentication, and redirect home to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-5xl">Hola mundo</h1>
-      <p className="text-2xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem illum ex maxime odio, asperiores, at laboriosam illo dolores mollitia numquam blanditiis similique. Tempore eius ea sed mollitia qui sunt sequi repudiandae iusto?
-      </p>
-    </main>
-  );
+  redirect("/dashboard")
 }
